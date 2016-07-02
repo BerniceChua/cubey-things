60c94a43ced34370c5588e018020fd3ab49ec243
BerniceChua <Bernice.Chua.415@gmail.com>
2016-07-01 17:43:13 -0700

    First Commit

---
+++ Assets/Scripts/CreateCubes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCubes : MonoBehaviour {
+
+	private GameObject randomCube = (GameObject)Instantiate(Resources.Load("RandomCube");
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
