829a659188ba021223830a253149f13fbb47891a
BerniceChua <Bernice.Chua.415@gmail.com>
2016-08-13 20:41:33 -0700

    Add script to allow object to be destroyed after a few seconds.

--- Assets/Scripts/CreateCubes.cs
+++ Assets/Scripts/CreateCubes.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using Random = System.Random;
+
 public class CreateCubes : MonoBehaviour {
 
+	private Random random = new Random ();
 	private GameObject randomCube = (GameObject)Instantiate(Resources.Load("RandomCube"));
 
 	// Use this for initialization
 	void Start () {
-		
+		for (int i = 0; i < 5; i++) {
+			Instantiate(randomCube);
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/DestroyThisRandomCube.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyThisRandomCube : MonoBehaviour {
+	public float delayUntilDestroy = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, delayUntilDestroy);
+	}
+}
