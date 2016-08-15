85e0d7a556b7d05c307189002df9244c0c115f54
BerniceChua <Bernice.Chua.415@gmail.com>
2016-08-15 11:12:39 -0700

    Add code to randomize cube sizes.

---
+++ Assets/Scripts/SizeRandomizer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SizeRandomizer : MonoBehaviour {
+	void Start () {
+		float cubeSize = Random.Range(0.001f, 2);
+		transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+	}
+}
